c7f52bf407ced8aec96e7adf90c7e50073201bb3
Vitor <96vitorleite@gmail.com>
2025-01-19 11:31:37 -0300

    feat: start user routers

---
+++ src/routes/userRoute.ts
@@ -0,0 +1,9 @@
+import express from "express";
+import { newUser } from "../controllers/userController";
+
+const router = express.Router();
+
+
+router.post("/newuser", newUser);
+
+export default router;
