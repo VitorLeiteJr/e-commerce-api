4a89fe2e0db6c48f206d07362f5e82763b2f7fb4
Vitor <96vitorleite@gmail.com>
2025-01-20 22:15:18 -0300

    refact: new organizational basis

---
+++ src/controllers/user/signup.ts
@@ -0,0 +1,28 @@
+
+import {Request, Response} from "express";
+import { db } from "../../lib/prisma";
+
+
+
+// simple register. I do not implement bcrypt
+export const signup = async (req: Request, res: Response) =>
+{
+    const {email,password,name} = await req.body;
+    
+    try{
+
+        const user = await db.user.createMany({
+            data:{
+                email,
+                password,
+                name
+            }
+        
+        })
+       res.send({status:true, message:"user created successfully"})
+
+    }catch{
+        res.send({status:false, message:"user not created"})
+
+    }
+};
