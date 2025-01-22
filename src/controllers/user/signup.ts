c27515386d531c9746d3a33d8496e60d7150e683
Vitor <96vitorleite@gmail.com>
2025-01-21 21:34:43 -0300

    feat: add bcrypt  and ensure when user registered that password be encrypted

--- src/controllers/user/signup.ts
+++ src/controllers/user/signup.ts
@@ -1,20 +1,26 @@
 
 import {Request, Response} from "express";
 import { db } from "../../lib/prisma";
+import bcrypt from "bcrypt";
 
 
 
-// simple register. I do not implement bcrypt
+const saltRounds = 10;
+
 export const signup = async (req: Request, res: Response) =>
 {
     const {email,password,name} = await req.body;
     
     try{
 
-        const user = await db.user.createMany({
+        const salt = bcrypt.genSaltSync(saltRounds);
+        const hashPassword = bcrypt.hashSync(password, salt);
+
+
+            await db.user.createMany({
             data:{
                 email,
-                password,
+                password: hashPassword,
                 name
             }
         
