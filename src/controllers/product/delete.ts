9ff8b192af0ff46fb1325e06a60319b3ff38418e
Vitor <96vitorleite@gmail.com>
2025-01-22 22:22:11 -0300

    feat: route to delete a product

---
+++ src/controllers/product/delete.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+import { db } from "../../lib/prisma";
+
+
+
+export const deleteProduct = async(req: Request, res: Response)=>{
+
+    const {id} = req.body;
+
+    
+    try{
+        await db.products.delete({
+            where: {
+                id
+            }
+        })  
+        res.send({status: true, message: "product deleted successfully!"})
+    }catch{
+        res.send({status: false, message:"Sometimes erros happen in your life, fix it!"});
+    }
+    
+
+    }
+
--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -1,9 +1,11 @@
 import express from "express"
 import { add } from "../controllers/product/add";
+import { deleteProduct } from "../controllers/product/delete";
 
 const router = express.Router();
 
 
 router.post('/add', add);
+router.post('/delete',deleteProduct);
 
 export default router;
