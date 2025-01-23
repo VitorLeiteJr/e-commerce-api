25286ee9baa14932261d62188330a339e014f426
Vitor <96vitorleite@gmail.com>
2025-01-22 22:27:34 -0300

    feat: route update info

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -1,11 +1,13 @@
 import express from "express"
 import { add } from "../controllers/product/add";
 import { deleteProduct } from "../controllers/product/delete";
+import { update } from "../controllers/product/update";
 
 const router = express.Router();
 
 
 router.post('/add', add);
 router.post('/delete',deleteProduct);
+router.post('/update',update);
 
 export default router;
