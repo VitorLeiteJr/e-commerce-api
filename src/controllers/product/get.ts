933358e47a6e6355226b126d1b5d98582fa3ceeb
Vitor <96vitorleite@gmail.com>
2025-01-25 18:51:22 -0300

    feat: ensure product get all products and one by id

--- src/controllers/product/get.ts
+++ src/controllers/product/get.ts
@@ -2,10 +2,15 @@ import { Request ,Response} from "express"
 import { db } from "../../lib/prisma";
 
 export const get = async (req: Request,res: Response  ) =>{
+    const {id} = req.body;
 
     try{
-       const all = await db.products.findMany();
-        res.send(all);    
+       const items = await db.products.findMany({
+        where:{
+            id: id
+        }
+       });
+        res.send(items);    
 
     }catch{
         res.send({status: false, message:"Sometimes erros happen in your life, fix it!"})
