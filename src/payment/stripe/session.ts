4f8c5bc1033934a5e9c529a7b6e81b87aece9a87
Vitor <96vitorleite@gmail.com>
2025-01-26 21:34:47 -0300

    feat: ensure get params from schema cart to get the items line to Stripe Session

--- src/payment/stripe/session.ts
+++ src/payment/stripe/session.ts
@@ -1,23 +1,38 @@
 import {Request, Response} from 'express';
 import Stripe from 'stripe';
+import { db } from '../../lib/prisma';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export const paymentSession = async(req: Request, res: Response) => {
     
-    
+    const {userId}  = req.body;
+
     try
     {
+
+                    const line_items = await db.cart.findMany({
+                        where: {
+                            userId
+                        },
+                        select:{
+                            price: true,
+                            quantity: true
+                        }
+                    });
+                    console.log(line_items);
+
+
                     const session = await stripe.checkout.sessions.create({
                     
-                        line_items: [
-                            {
+                        line_items: line_items,//[
+                            // {
                             // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-                            price: 'price_1Q8l0vFIO4BK2zy8z5g6xqk3',
-                            quantity: 1,
-                            },
-                        ],
-                        mode: 'subscription',
+                            // price: 'price_1Q8l0vFIO4BK2zy8z5g6xqk3',
+                            // quantity: 1,
+                            // },
+                        //],
+                        mode: 'payment',
                         success_url: 'http://localhos:3000/?success=true',
                         cancel_url: 'http://localhost:3000/?canceled=true',
 
@@ -27,9 +42,10 @@ export const paymentSession = async(req: Request, res: Response) => {
 
                         res.send({url: session.url});
                     
-    }catch
+    }catch(error)
     {
         res.send({status: "error"});
+        console.log(error);
     }
 
 
