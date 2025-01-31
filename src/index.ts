3340617f25cd724128813ffc9d35277c01ca84ef
Vitor <96vitorleite@gmail.com>
2025-01-30 22:21:01 -0300

    fix: ajust webhook route to use raw body

--- src/index.ts
+++ src/index.ts
@@ -4,19 +4,22 @@ import cors from "cors";
 import productRoutes from "./routes/productRoutes";
 import cartRoutes from "./routes/cartRoutes";
 import paymentRoutes from "./routes/paymentRoutes";
+import  webhook from "./payment/webhook/routes";
 
 
 const app = express();
 const port = 3000;
 
-app.use(express.json());
+
 app.use(cors()); // Enable CORS
 
+app.use("/api/webhook", webhook)
+
+app.use(express.json());
 app.use("/api/user", userRoutes);
 app.use("/api/product", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/payment", paymentRoutes);
-
 
 
 app.listen(port);
