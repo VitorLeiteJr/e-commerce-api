37a3292994e1ccbff21a8e5818c85e78eb998d74
Vitor <96vitorleite@gmail.com>
2025-02-01 23:58:36 -0300

    fix: add statement to login auth

--- src/controllers/user/login.ts
+++ src/controllers/user/login.ts
@@ -1,14 +1,15 @@
 import {Request, Response} from "express"
 import jwt from "jsonwebtoken"
-import { db } from "../../lib/prisma";
 
 
 export const logIn = async (req: Request, res: Response)=>{
     
     const {email, password} = req.body;
 
-   const token = jwt.sign({data:{email, password}},"secretkey", {expiresIn: '1H'});
+   try{const token = jwt.sign({data:{email, password}},"secretkey", {expiresIn: '1H'});
 
     res.send({token});
-    
+    }catch{
+        res.send({message:"sometimes go wrong" })
+    }
 };
