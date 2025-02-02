2c1d3bd6b02fdb132e7b6b1a29aa7454aa26ec62
Vitor <96vitorleite@gmail.com>
2025-02-02 19:55:21 -0300

    fix: fix statement from login auth

--- src/controllers/user/login.ts
+++ src/controllers/user/login.ts
@@ -1,15 +1,36 @@
 import {Request, Response} from "express"
 import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt";
+import { db } from "../../lib/prisma";
 
 
 export const logIn = async (req: Request, res: Response)=>{
     
     const {email, password} = req.body;
+    
+    
+    try{
+    
+    const credentials = await db.user.findFirst({
+        where:{
+            email: email,
+        }
+    });
 
-   try{const token = jwt.sign({data:{email, password}},"secretkey", {expiresIn: '1H'});
 
-    res.send({token});
+    console.log(credentials)
+
+    const match =  await bcrypt.compare(password, credentials.password);
+
+    if(!match) {
+                        res.send({message: "credentials wrong"});
+                        return;
+                }
+
+   const token = jwt.sign({data:{email, password}},"secretkey", {expiresIn: '1H'});
+
+   res.send({token});
     }catch{
-        res.send({message:"sometimes go wrong" })
+        res.send({message:"sometimes go wrong" });
     }
-};
+ };
